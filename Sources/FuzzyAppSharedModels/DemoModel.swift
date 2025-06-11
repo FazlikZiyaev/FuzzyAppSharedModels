59e2ddb9fd92eb1bc8b882c7db7f607298d518da
FazlikZiyaevPayme <fazliddin.z@payme.uz>
2025-06-11 16:53:38 +0500

    first reusable model

---
+++ Sources/FuzzyAppSharedModels/DemoModel.swift
@@ -0,0 +1,11 @@
+//
+//  DemoModel.swift
+//  FuzzyAppSharedModels
+//
+//  Created by Fazlik Ziyaev on 11/06/25.
+//
+
+public struct DemoModel {
+    public var name: String
+    public var age: Int
+}
